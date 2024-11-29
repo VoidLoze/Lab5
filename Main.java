140a1839dc5a4d1f8f1f52504f832095a32dd10d
VoidLoze <nik.n.novikov.2003@gmail.com>
2024-11-29 17:36:20 +0300

    change four zadanie 1

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ class Main{
         System.out.println("изменение один");
         System.out.println("изменение два");
         System.out.println("изменение три");
+        System.out.println("изменение четыре");
     }
 }
