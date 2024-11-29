3e161e34bbfac05271512bb473e8ad298636656b
VoidLoze <nik.n.novikov.2003@gmail.com>
2024-11-29 18:45:11 +0300

    ddd

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] args) {
+        System.out.print("Это добавлено в ветке main");
+        System.out.println("изменение один");
+        System.out.println("изменение два");
+        System.out.println("изменение три");
+        System.out.println("изменение четыре");
+    }
+}
