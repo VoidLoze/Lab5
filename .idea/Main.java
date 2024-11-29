e63b0c691c07e0383bbd47b8dcf33143a4a0209e
VoidLoze <nik.n.novikov.2003@gmail.com>
2024-11-29 18:17:10 +0300

    two zadanie dva

--- .idea/Main.java
+++ .idea/Main.java
@@ -3,5 +3,6 @@ class Main{
         System.out.print("Это добавлено в ветке dev");
         System.out.print("изменение 1");
         System.out.print("изменение 2");
+        System.out.print("изменение 3");
     }
 }
