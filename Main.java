0287e9e3a563909bce34e7a6bf63ddbd67b95611
VoidLoze <nik.n.novikov.2003@gmail.com>
2024-11-29 17:35:57 +0300

    change three zadanie 1

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main{
         System.out.print("Это добавлено в ветке main");
         System.out.println("изменение один");
         System.out.println("изменение два");
+        System.out.println("изменение три");
     }
 }
